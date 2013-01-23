939d580eb5d9db6697e7e82978361fa0366241a6
Grant Spilsbury <grantspilsbury@gmail.com>
2013-01-23 13:08:10 -0800

    Make a basic user model including password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(name: "Example user", email: "example@example.com", password: "password", password_confirmation: "password") }
+  subject { @user }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  
+  it { should be_valid }
+  
+  describe "when name is not present" do
+    before { @user .name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when email is not present" do
+    before { @user .email = " " }
+    it { should_not be_valid }
+  end
+end
