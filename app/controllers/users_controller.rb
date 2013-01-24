2ee871ee1d1386c212d425a8c77421a64779a696
Grant Spilsbury <grantspilsbury@gmail.com>
2013-01-24 14:30:38 -0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+  
+  def index
+    @users = User.all
   end
 end
